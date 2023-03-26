06156b328e40ca6daeb5e3feb844a3dfb1d1529c
Basant kumar Hota <basahota@users.noreply.github.com>
2023-03-26 12:21:40 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/CsApplication.java
@@ -0,0 +1,17 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class CsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsApplication.class, args);
+	}
+
+}
